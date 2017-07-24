36e9a87fcd23ed175808314fb4f744e8c771a84e
Vicuko <victorc.fernandez@gmail.com>
2017-07-24 17:33:57 +0200

    Include song file in raw folder

--- app/src/main/java/com/example/android/mediaplayerapp/MainActivity.java
+++ app/src/main/java/com/example/android/mediaplayerapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.mediaplayerapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MediaPlayer mediapPlayer = MediaPlayer.create(this, R.raw.song);
+
+        Button rewind = (Button) findViewById(R.id.rewind);
+        Button play = (Button) findViewById(R.id.rewind);
+        Button pause = (Button) findViewById(R.id.rewind);
+        Button forward = (Button) findViewById(R.id.rewind);
+
+        rewind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        forward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
